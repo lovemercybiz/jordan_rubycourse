8cbea3237823664c5f73c77d28a76a3187e13985
Love Mercy <lovemercybiz@gmail.com>
2019-09-28 17:54:26 -0700

    Added sample data to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |b|
+        Blog.create!(
+            title:  "Blog post #{b}",
+            body: "Ut tincidunt at ipsum nec semper. Fusce in risus augue. Nullam ligula dolor, hendrerit vitae tempor et, interdum at augue. Aenean felis magna, eleifend eu facilisis ut, efficitur vel leo. Etiam non turpis et sapien pellentesque egestas. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec eu malesuada leo. Phasellus tempor vel neque sit amet bibendum. Suspendisse interdum justo purus, in lacinia justo sollicitudin a. Vivamus convallis volutpat odio, quis ultrices velit vestibulum et. Proin condimentum enim laoreet facilisis rhoncus. Integer vitae congue tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque laoreet arcu non neque vulputate ornare. "
+        )
+end
+puts "10 blog posts created"
+
+5.times do |s|
+        Skill.create!(
+            title: "Skill ##{s}",
+            percent_utilized: s + 10
+        )
+end
+puts "5 skills created"
+
+9.times do |p|
+    Portfolio.create!(
+    title: "Portfolio title #{p}",
+    subtitle: "Great service #{p}",
+    body: "Ut dapibus consectetur dolor vel cursus. Aliquam fermentum ex molestie turpis faucibus, eget commodo nunc aliquam. Pellentesque volutpat eros ante, a vehicula urna lobortis quis. Aliquam nec consequat nibh. In lorem tellus, auctor posuere augue non, ullamcorper auctor velit. Vivamus congue sed mauris id malesuada. Cras blandit euismod risus, ut consequat felis faucibus id. Nulla eu purus massa. Integer laoreet dui velit, quis ullamcorper erat aliquam sit amet.",
+    main_image: "https://via.placeholder.com/300x250/fff.png",
+    thumb_image: "https://via.placeholder.com/50x50/fff.png"
+    )
+end
+puts "9 portfolios items created"
