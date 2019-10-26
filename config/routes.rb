545f1a3f05e1077b2e8f71ca8acc4a8aaa624224
Love Mercy <lovemercybiz@gmail.com>
2019-10-26 07:10:54 -0700

    Customized portfolio routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   # get 'pages/home'
   get 'about-me', to: 'pages#about' # this changes  http://localhost:3000/pages/about >> http://localhost:3000/about
   #get 'about', to: 'pages#about' # changing the string after 'get' changes the url in browser, thats how you get custom routes.
