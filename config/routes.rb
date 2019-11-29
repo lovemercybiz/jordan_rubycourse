763f5a5716fe2466879a9594cc6505126e78c916
Love Mercy <lovemercybiz@gmail.com>
2019-11-29 16:53:38 -0500

    Implemented custom methods for updating status

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,13 @@ Rails.application.routes.draw do
   get 'about-me', to: 'pages#about' # this changes  http://localhost:3000/pages/about >> http://localhost:3000/about
   #get 'about', to: 'pages#about' # changing the string after 'get' changes the url in browser, thats how you get custom routes.
   get 'contact', to: 'pages#contact'
-  resources :blogs
+
+
+  resources :blogs do
+      member do
+        get :toggle_status
+      end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'pages#home' #this changed the orginal home page (that you're on rails) to blog's home
