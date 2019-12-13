71b96889c19ca4b2781db9ea84885c072d875508
Love Mercy <lovemercybiz@gmail.com>
2019-12-13 08:34:28 -0500

    Completed authentication with virtual attributes for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
   # :omniauthable - Adds third-party login options ex: log in with facebook
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+
+        validates_presence_of :name
+
+         def first_name
+            self.name.split.first
+         end 
+         
+         def last_name
+          self.name.split.last
+        end
 end
