6cb10e716cadbbda0206ba054053c4eeefc6e600
Love Mercy <lovemercybiz@gmail.com>
2019-12-22 17:56:10 -0500

    Made topics optional for blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -6,7 +6,7 @@ class Blog < ApplicationRecord
 
     validates_presence_of :title, :body
 
-     belongs_to :topic #this prevented blogs from being created cause it required topics to be added
+     belongs_to :topic, optional: true #this prevented blogs from being created cause it required topics to be added
 
     def self.featured_blogs
             limit(2)
