9ca6a2babb4d91cc23a016ef3c8a53c6ac1f2dad
Love Mercy <lovemercybiz@gmail.com>
2019-12-24 11:16:34 -0500

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,42 @@ module ApplicationHelper
         LoveViewTool::Renderer.copyright 'Love Mercy', 'All rights reserved'
     end
 
+def nav_items
+        [
+            {
+                url: root_path,
+                title: "Home"
+            },
+            {
+                url: about_me_path,
+                title: "About Me"
+            },
+            {
+                url: contact_path,
+                title: "Contact"
+            },
+            {
+                url: portfolios_path,
+                title: "Portfolios"
+            },
+            {
+                url: blogs_path,
+                title: "Blogs"
+            },
+        ]
+end
+
+    def nav_helper(style, tag_type)
+        nav_links = ''
+
+        nav_items.each do |item|
+            nav_links <<   "<#{tag_type}><a href='#{item[:url]}' class = '#{style} #{active?(item[:url])}'>#{item[:title]}</a></#{tag_type}>"      
+        end
+
+                nav_links.html_safe
+    end
+
+    def active?(path)
+        "active" if current_page? path
+    end
 end
