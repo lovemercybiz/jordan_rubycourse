0dd32aafc7683c69042bb18e0cbc474a060cf3c0
Love Mercy <lovemercybiz@gmail.com>
2019-12-26 22:21:28 -0500

    Updated file upload forms and refactored placeholder functionality into view

--- app/models/concerns/placeholder.rb
+++
@@ -1,7 +0,0 @@
-module Placeholder
-        extend ActiveSupport::Concern
-
-        def self.image_generator(height:, width:)
-             "https://via.placeholder.com/#{height}x#{width}/F39A9E.png"
-        end
-end
