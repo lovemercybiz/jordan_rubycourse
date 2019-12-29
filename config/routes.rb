f6f6ca56712e54ee233f624b4d4ae5718c56dd33
Love Mercy <lovemercybiz@gmail.com>
2019-12-29 07:30:48 -0500

    Created comment broadcast, configured websocket roucket, and updated the comment model with a callback

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,6 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  mount ActionCable.server => '/cable'
   root to: 'pages#home' #this changed the orginal home page (that you're on rails) to blog's home
 end
